1dfbc2127c505ca2cf18bcf0bb14b4734603a0ba
Kalle Lindqvist <kalle.lindqvist@yutani.io>
2022-03-12 15:32:52 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-docker build -t konstare/emacs-gcc-pgtk:latest .
+docker build -t ellakk/emacs-gcc-pgtk:latest .
 id=$(docker create ellakk/emacs-gcc-pgtk)
 docker cp $id:/opt/deploy .
